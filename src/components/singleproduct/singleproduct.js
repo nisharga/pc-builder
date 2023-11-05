14c9372667a2da4f816a4f2b3c29746c200b9959
Nisharga Kabir <kabirnisharga@gmail.com>
2023-11-05 23:39:33 +0600

    pc builder page done

--- src/components/singleproduct/singleproduct.js
+++ src/components/singleproduct/singleproduct.js
@@ -1,7 +1,17 @@
+import { addToCartRam, removeFromCartRam } from "@/redux/ram/ramSlice";
 import Image from "next/image";
 import Link from "next/link";
+import { useRouter } from "next/router";
+import { useDispatch } from "react-redux";
 const Singleproduct = ({ data }) => {
   const { img, name, price, rating, status, category } = data;
+  const dispatch = useDispatch();
+  const router = useRouter();
+  const handleAdd = (data) => {
+    dispatch(removeFromCartRam(data));
+    dispatch(addToCartRam(data));
+    router.push("/pcbuilder");
+  };
   return (
     <div className="block rounded-xl border shadow-xl transition hover:border-pink-500/10 hover:shadow-pink-500/10 relative lg:col-span-4 md:col-span-6 col-span-12">
       <div className="bg-red-500 absolute py-1 px-2 rounded-r-lg mt-10">
@@ -37,15 +47,16 @@ const Singleproduct = ({ data }) => {
           </p>
         </div>
         <div className="flex justify-between py-3">
-          <a
+          <button
             disabled={status != "available" ? true : false}
             className={`inline-block rounded border border-current px-8 py-3 text-sm font-medium text-indigo-600 transition focus:outline-none focus:ring active:text-indigo-500 ${
               status !== "available" ? "" : "hover:scale-110 hover:shadow-xl"
             }`}
             href="/download"
+            onClick={() => handleAdd(data)}
           >
             Add To Builder
-          </a>
+          </button>
           <a
             class="inline-flex items-center gap-2 rounded border border-indigo-600 bg-indigo-600 px-8 py-3 text-white hover:bg-transparent hover:text-indigo-600 focus:outline-none focus:ring active:text-indigo-500"
             href="/download"
--- src/pages/pcbuilder.js
+++ src/pages/pcbuilder.js
@@ -1,13 +1,17 @@
 import SectinonBannar from "@/components/shared/sectionbannar/sectionbannar";
+import RootLayout from "@/layout/layout";
 import Image from "next/image";
 import { BsFillCpuFill } from "react-icons/bs";
-// import {BsFillMotherboardFill } from "react-icons/bs"; = motherboard
+import { BsFillMotherboardFill } from "react-icons/bs";
+import { useSelector } from "react-redux";
 // import { FaMemory} from "react-icons/fa"; = ram
 // import { ImPowerCord} from "react-icons/im"; = power
 // import { MdSdStorage } from "react-icons/md"; = stroage
 // import { MdMonitor } from "react-icons/md"; = monitor
 
 const Pcbuilder = () => {
+  const { ramproducts } = useSelector((state) => state.ram);
+  console.log(ramproducts);
   return (
     <div>
       <SectinonBannar
@@ -17,13 +21,13 @@ const Pcbuilder = () => {
       />
       <div className="mx-10 mt-12">
         <div className="grid grid-cols-12">
-          <div className="lg:col-span-2 col-span-4 bg-gray-500 text-[#fff] flex items-center font-2xl p-2">
+          <div className="lg:col-span-2 col-span-3 bg-gray-500 text-[#fff] flex items-center font-2xl p-2">
             <span className="text-4xl">
               <BsFillCpuFill />
             </span>
             <span className="ml-2 text-2xl">Ram</span>
           </div>
-          <div className="col-span-8 flex items-center ml-2">
+          <div className="lg:col-span-7 col-span-6 flex items-center ml-2">
             <Image
               src="https://www.startech.com.bd/image/cache/catalog/ram/crucial/8gb-ddr5/8gb-ddr5-500x500.webp"
               alt="image"
@@ -34,9 +38,42 @@ const Pcbuilder = () => {
               Intel Pentium Gold G7400 Alder Lake Processor
             </h3>
           </div>
-          <div className="col-span-2">
-            <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-              Button
+          <div className="col-span-1 flex items-center">
+            <h3 className="font-bold">{15000} TK</h3>
+          </div>
+
+          <div className="col-span-2 flex items-center">
+            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+              Choose
+            </button>
+          </div>
+        </div>
+
+        <div className="grid grid-cols-12 mt-3">
+          <div className="lg:col-span-2 col-span-3 bg-gray-500 text-[#fff] flex items-center font-2xl p-2">
+            <span className="lg:text-4xl text-lg">
+              <BsFillMotherboardFill />
+            </span>
+            <span className="ml-2 lg:text-2xl text-md">MotherBoard</span>
+          </div>
+          <div className="lg:col-span-7 col-span-6 flex items-center ml-2">
+            <Image
+              src="https://www.startech.com.bd/image/cache/catalog/ram/crucial/8gb-ddr5/8gb-ddr5-500x500.webp"
+              alt="image"
+              width={70}
+              height={70}
+            />
+            <h3 className="font-bold ml-3">
+              Intel Pentium Gold G7400 Alder Lake Processor
+            </h3>
+          </div>
+          <div className="col-span-1 flex items-center">
+            <h3 className="font-bold">{15000} TK</h3>
+          </div>
+
+          <div className="col-span-2 flex items-center">
+            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+              Choose
             </button>
           </div>
         </div>
@@ -46,3 +83,7 @@ const Pcbuilder = () => {
 };
 
 export default Pcbuilder;
+
+Pcbuilder.getLayout = function getLayout(page) {
+  return <RootLayout>{page}</RootLayout>;
+};
