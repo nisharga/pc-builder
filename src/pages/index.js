01018608e8c9023603c2f08cd2c151ac0062c7c9
Nisharga Kabir <kabirnisharga@gmail.com>
2023-11-07 17:00:41 +0600

    Feature product section add

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,31 +1,33 @@
-import RootLayout from "@/layout/layout";
-
-import { Inter } from "next/font/google";
-// import Bannar from "./../components/bannar/bannar";
 import SingleCategory from "@/components/singleCategory/singleCategory";
 import Singleproduct from "@/components/singleproduct/singleproduct";
 import Bannar from "@/components/bannar/bannar";
+import RootLayout from "@/layout/layout";
+import Featuredproduct from "@/components/featuredProduct/FeaturedProduct";
 
 export default function Home({ allProduct }) {
-  const shuffle = (arr) => [...arr].sort(() => Math.random() - 0.5);
-  const randomSixData = shuffle(allProduct);
-  console.log(randomSixData.length);
+  /* const shuffle = (arr) => [...arr].sort(() => Math.random() - 0.5);
+  const randomSixData = shuffle(allProduct); */
+  // console.log(randomSixData.length, "six");
+  // console.log(allProduct?.length);
   return (
     <>
       <Bannar />
-      {/*  <div className="md:w-3/12 mx-auto text-center mt-12 mb-10">
+      <Featuredproduct allProduct={allProduct} />
+      {/* <div className="md:w-3/12 mx-auto text-center mt-12 mb-10">
         <h4 className=" mb-3 text-lg">Check & Get Your Desired Product!</h4>
         <h2 className="text-3xl uppercase border-y-4 py-3 text-[#0000ff]">
           Featured Products
         </h2>
       </div>
       <div className="grid grid-cols-12 gap-6 mx-20">
-        {randomSixData?.slice(0, 6)?.map((data) => (
-          <Singleproduct data={data} key={data._id} />
-        ))}
-      </div>
+        {randomSixData
+          ? randomSixData.map((data) => (
+              <Singleproduct data={data} key={data._id} />
+            ))
+          : "Loading"}
+      </div> */}
 
-      <div className="container">
+      {/* <div className="container">
         <div className="md:w-3/12 mx-auto text-center mt-12 mb-10">
           <h4 className=" mb-3 text-lg">Exclusive</h4>
           <h2 className="text-3xl uppercase border-y-4 py-3 text-[#0000ff]">
@@ -37,8 +39,7 @@ export default function Home({ allProduct }) {
             <SingleCategory key={value.id} value={value} />
           ))}
         </div>
-      </div>
- */}
+      </div> */}
     </>
   );
 }
@@ -56,6 +57,5 @@ export const getStaticProps = async () => {
     props: {
       allProduct: data.data,
     },
-    revalidate: 3600,
   };
 };
